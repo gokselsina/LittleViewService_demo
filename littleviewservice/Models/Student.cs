8bbe743b15dec89e1f31a868ffcd5e6b2c753967
goksel sina <goksel.sina@hotmail.com>
2023-03-12 00:08:13 +0300

    Added return image feature.

--- littleviewservice/Models/Student.cs
+++ littleviewservice/Models/Student.cs
@@ -1,13 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace littleviewservice.Models
 {
     public class Student
     {
-        public int ID { get; } 
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public char Gender { get; set; }
 
+        //public string Img { get; set; }
     }
 }
