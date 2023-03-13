b38e846b9c279be71d0656b4396f9fd41d0461dc
goksel sina <goksel.sina@hotmail.com>
2023-03-13 23:28:03 +0300

    Added return parents and more features.

--- littleviewservice/Models/Student.cs
+++ littleviewservice/Models/Student.cs
@@ -5,10 +5,10 @@ namespace littleviewservice.Models
 {
     public class Student
     {
-        public int ID { get; set; }
+        public int? ID { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public char Gender { get; set; }
+        public char? Gender { get; set; }
         public string? Blood_type { get; set; }
         public DateTime? Birth_date { get; set; } = DateTime.MinValue;
         public string? Parent_1 { get; set; } = null;
