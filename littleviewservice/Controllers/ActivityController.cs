258e21597c920bf205c0042d6de10c0028397300
goksel sina <goksel.sina@hotmail.com>
2023-03-20 23:59:05 +0300

    Added add activity service.

--- littleviewservice/Controllers/ActivityController.cs
+++ littleviewservice/Controllers/ActivityController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Globalization;
+using static littleviewservice.Controllers.StudentController;
 
 namespace littleviewservice.Controllers
 {
@@ -20,11 +22,40 @@ namespace littleviewservice.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Activity>>> GetActivity()
         {
-            if (_dbContext.tbl_activity == null)
+            var actList = await _dbContext.tbl_activity
+                .Where(n => n.date >= DateTime.Today)
+            .OrderBy(n => n.date)  // date sütununa göre tersine sırala
+            .ToListAsync();
+
+            if (actList == null || !actList.Any())
             {
                 return NotFound();
             }
-            return await _dbContext.tbl_activity.ToListAsync();
+
+            return actList;
+
         }
+        [HttpPost("addActivity")]
+        public async Task<IActionResult> AddActivityAsync([FromBody] ActivityCredentials credentials)
+        {
+            Activity act = new Activity
+            {
+                date = credentials.date,
+                title = credentials.title,
+                text = credentials.text
+            };
+
+            _dbContext.tbl_activity.Add(act);
+            await _dbContext.SaveChangesAsync();
+            return Ok("Inserted!");
+        }
+
+        public class ActivityCredentials
+        {
+            public DateTime? date { get; set; } = DateTime.MinValue;
+            public string? title { get; set; }
+            public string? text { get; set; }
+        }
+
     }
 }
