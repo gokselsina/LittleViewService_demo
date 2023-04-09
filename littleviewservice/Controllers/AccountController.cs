c3af41f87adf46dc0257e1d26c19efe08c56beb1
goksel sina <goksel.sina@hotmail.com>
2023-04-09 22:37:09 +0300

    Updated account service. Added add account and update account services.

--- littleviewservice/Controllers/AccountController.cs
+++ littleviewservice/Controllers/AccountController.cs
@@ -48,6 +48,23 @@ namespace littleviewservice.Controllers
 
         }
 
+        [HttpGet("getParentAccounts")]
+        public async Task<ActionResult<List<Account>>> GetParentAccounts()
+        {
+            var AccountList = await _dbContext.tbl_account
+            .Where(n => n.account_type == 1)
+            .OrderByDescending(n => n.ID)  // Send_date sütununa göre tersine sırala
+            .ToListAsync();
+
+            if (AccountList == null || !AccountList.Any())
+            {
+                return NotFound();
+            }
+
+            return AccountList;
+
+        }
+
         [HttpPost("addAccount")]
         public async Task<IActionResult> AddAccountAsync([FromBody] AccountCredentials credentials)
         {
@@ -57,13 +74,40 @@ namespace littleviewservice.Controllers
                 password = credentials.Password,
                 name = credentials.Name,
                 surname = credentials.Surname,
-                account_type = credentials.Account_type
+                account_type = credentials.Account_type,
+                class_id = credentials.Class_id
             };
 
             _dbContext.tbl_account.Add(acc);
             await _dbContext.SaveChangesAsync();
             return Ok("Inserted!");
         }
+
+        [HttpPut("updateAccount/{id}")]
+        public async Task<IActionResult> UpdateStudentAsync(int id, [FromBody] AccountCredentials credentials)
+        {
+            var acc = await _dbContext.tbl_account.FindAsync(id);
+            if (acc == null) return NotFound();
+
+            acc.username = credentials.Username;
+            acc.name = credentials.Name;
+            acc.surname = credentials.Surname;
+            acc.password = credentials.Password;
+            acc.class_id = credentials.Class_id;
+            acc.account_type = credentials.Account_type;
+
+            _dbContext.tbl_account.Update(acc);
+            await _dbContext.SaveChangesAsync();
+            return Ok("Updated!");
+        }
+
+        [HttpGet("countUsername/{username}")]
+        public async Task<IActionResult> CountUsernamesAsync(string username)
+        {
+            int count = await _dbContext.tbl_account.CountAsync(n => n.username ==  username);
+            return Ok(count);
+        }
+
 
         [HttpPost("login")]
         public async Task<ActionResult<Account>> Login([FromBody] LoginRequest request)
@@ -93,6 +137,7 @@ namespace littleviewservice.Controllers
             public string Name { get; set; }
             public string Surname { get; set; }
             public int Account_type { get; set; }
+            public int? Class_id { get; set; }
         }
     }
 }
--- littleviewservice/Models/Account.cs
+++ littleviewservice/Models/Account.cs
@@ -8,6 +8,7 @@
         public int? account_type { get; set; }
         public string? name { get; set; }
         public string? surname { get; set; }
+        public int? class_id { get; set; }
 
     }
 }
