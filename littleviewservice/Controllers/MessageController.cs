45afd125c05a2196341da60e4df018cba3c2c8b3
goksel sina <goksel.sina@hotmail.com>
2023-04-13 00:00:05 +0300

    Added message service

---
+++ littleviewservice/Controllers/MessageController.cs
@@ -0,0 +1,134 @@
+﻿using littleviewservice.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static littleviewservice.Controllers.MessageController;
+
+namespace littleviewservice.Controllers
+{
+    [Route("LittleService/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly LittleContext _dbContext;
+
+        public MessageController(LittleContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Message>>> GetNotification()
+        {
+            if (_dbContext.tbl_notification == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.tbl_message.ToListAsync();
+        }
+
+        [HttpGet("{send_to}")]
+        public async Task<ActionResult<List<Message>>> GetMessage(int send_to)
+        {
+            var msgList = await _dbContext.tbl_message
+            .Where(n => n.Send_to == send_to || n.Send_to == null)
+            .OrderByDescending(n => n.Send_date)  // Send_date sütununa göre tersine sırala
+            .ThenByDescending(n => n.Unread) // Unread sütununa göre tersine sırala
+            .ToListAsync();
+
+            if (msgList == null || !msgList.Any())
+            {
+                return NotFound();
+            }
+
+            return msgList;
+            
+        }
+
+        [HttpGet("countUnreadMessages/{send_to}")]
+        public async Task<IActionResult> CountUnreadMessagesAsync(int send_to)
+        {
+            int count = await _dbContext.tbl_message.CountAsync(n => n.Send_to == send_to && n.Unread);
+            return Ok(count);
+        }
+
+        [HttpPost("sendMessage")]
+        public async Task<IActionResult> SendMessage([FromBody] MessageCredentials credentials)
+        {
+
+            Message msg = new Message
+            {
+                Text = credentials.Text,
+                Send_from = credentials.Send_from,
+                Send_to = credentials.Send_to,
+                Send_date = credentials.Send_date,
+                Unread = true,
+            };
+
+            _dbContext.tbl_message.Add(msg);
+            await _dbContext.SaveChangesAsync();
+            return Ok("Inserted!");
+        }
+
+        
+        [HttpGet("Alarms/{send_to}")]
+        public async Task<ActionResult<List<Message>>> GetAlarms(int send_to)
+        {
+            var msgList = await _dbContext.tbl_message
+            .Where(n => (n.Send_to == send_to || n.Send_to == null) && n.Alarm)
+            .OrderByDescending(n => n.Send_date)  // Send_date sütununa göre tersine sırala
+            .ThenByDescending(n => n.Unread) // Unread sütununa göre tersine sırala
+            .ToListAsync();
+
+            if (msgList == null || !msgList.Any())
+            {
+                return NotFound();
+            }
+
+            return msgList;
+
+        }
+ 
+        [HttpPut("markAsRead/{send_to}/{send_from}")]
+        public async Task<IActionResult> UpdateMessageAsync(int send_to, int send_from)
+        {
+            var msgList = await _dbContext.tbl_message.Where(n => n.Send_to == send_to && n.Send_from == send_from).ToListAsync();
+            if (msgList == null || msgList.Count == 0)
+            {
+                return NotFound();
+            }
+            foreach (var msg in msgList)
+            {
+                msg.Unread = false;
+                _dbContext.tbl_message.Update(msg);
+            }
+            await _dbContext.SaveChangesAsync();
+            return Ok("Updated!");
+        }
+
+        [HttpPut("markAsAlarmOff/{send_to}")]
+        public async Task<IActionResult> UpdateMessageAlarmAsync(int send_to)
+        {
+            var msgList = await _dbContext.tbl_message.Where(n => n.Send_to == send_to).ToListAsync();
+            if (msgList == null || msgList.Count == 0)
+            {
+                return NotFound();
+            }
+            foreach (var msg in msgList)
+            {
+                msg.Alarm = false;
+                _dbContext.tbl_message.Update(msg);
+            }
+            await _dbContext.SaveChangesAsync();
+            return Ok("Updated!");
+        }
+
+        public class MessageCredentials
+        {
+            public string Text { get; set; }
+            public int Send_from { get; set; }
+            public int? Send_to { get; set; }
+            public DateTime? Send_date { get; set; } = DateTime.MinValue;
+        }
+    }
+}
---
+++ littleviewservice/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace littleviewservice.Models
+{
+    public class Message
+    {
+        public int? ID { get; set; }
+        public string Text { get; set; }
+        public int Send_from { get; set; }
+        public int? Send_to { get; set; }
+        public DateTime? Send_date { get; set; } = DateTime.MinValue; 
+        public bool Unread { get; set; }
+        public bool Alarm { get; set; }
+    }
+}
